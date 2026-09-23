1a7ad9ba7165b36ca3bb97434cd7fbc2951ee647
Emiliano Gareca <kyrvenx@gmail.com>
2026-09-23 20:52:14 -0300

    Entrega del frontend MetalForma CNC

--- backend/routes/transportes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var transportesController = require('../controllers/transportesController');
-
-router.get('/', transportesController.mostrarEstado);
-router.get('/prueba-bd', transportesController.probarConexion);
-router.post('/contacto', transportesController.recibirContacto);
-
-module.exports = router;
